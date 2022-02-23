c0fd0e89c79bca88a13845bbb033f9da5e1dec4d
Adonis Edwards <adonisedwards@adoniss-mbp.myfiosgateway.com>
2022-02-23 13:32:07 -0500

    commit

--- src/components/ButtonStyle.js
+++ src/components/ButtonStyle.js
@@ -5,7 +5,7 @@ export const Wrapper = styled.div`
   bottom: 0;
   max-width: 100vw;
   height: auto;
-  margin-top: 40%;
+  margin-top: 39%;
   @media (max-width: 800px) {
     margin-top: 115%;
   }
