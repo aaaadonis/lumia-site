e0a10341d2ef5c2e02a5a0836aac7168b6f522da
Adonis Edwards <adonisedwards@adoniss-mbp.myfiosgateway.com>
2022-02-26 18:27:44 -0500

    commit

--- src/components/ButtonStyle.js
+++ src/components/ButtonStyle.js
@@ -4,7 +4,7 @@ import styled from "styled-components";
 export const Wrapper = styled.div`
   position: fixed;
   width: 100vw;
-  bottom: 15px;
+  bottom: 35px;
 `;
 
 export const Content = styled.div`
