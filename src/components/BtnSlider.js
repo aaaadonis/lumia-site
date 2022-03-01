e44fdb56a911295cc017e72f8ffee0e7be3219d4
Adonis Edwards <adonisedwards@adoniss-mbp.myfiosgateway.com>
2022-03-01 09:47:08 -0500

    commit

--- src/components/BtnSlider.js
+++ src/components/BtnSlider.js
@@ -2,16 +2,26 @@ import React from 'react'
 import './card.css'
 import leftArrow from '../images/left-arrow.svg'
 import rightArrow from '../images/right-arrow.svg'
+import { isMobile } from "react-device-detect";
+
 
 const BtnSlider = ({ direction, moveSlide }) => {
     return (
-    <button
-      onClick={moveSlide}
-      className={direction === "next" ? "btn-slide next" : "btn-slide prev"}
-    >
-      <img src={direction === "next" ? rightArrow : leftArrow} />
-    </button>
-    )
+      <button
+        onClick={moveSlide}
+        className={
+          isMobile
+            ? "off"
+            : direction === "next"
+            ? "btn-slide next"
+            : "btn-slide prev"
+        }
+      >
+        <img
+          src={isMobile ? "off" : direction === "next" ? rightArrow : leftArrow}
+        />
+      </button>
+    );
 }
 
 export default BtnSlider
