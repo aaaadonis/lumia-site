fddfc2469191ed306bc9a261f213a0f957c96d24
Adonis Edwards <adonisedwards@adoniss-mbp.myfiosgateway.com>
2022-04-04 14:36:34 -0400

    commit

--- src/components/BtnSlider.js
+++ src/components/BtnSlider.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import './card.css'
-import leftArrow from '../images/left-arrow.svg'
-import rightArrow from '../images/right-arrow.svg'
+
 import { isMobile } from "react-device-detect";
 
 
@@ -11,7 +10,6 @@ const BtnSlider = ({ direction, moveSlide }) => {
         onClick={moveSlide}
         className={direction === "next" ? "btn-slide next" : "btn-slide prev"}
       >
-        <img src={direction === "next" ? rightArrow : leftArrow} />
       </button>
     );
 }
